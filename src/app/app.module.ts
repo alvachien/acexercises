ce918bdad798894b59418c52a3cd5da4932db79c
Alva Chien <alvachien@163.com>
2017-10-24 11:49:38 +0800

    Setup the folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ItemTypeComponent } from './item-type/item-type.component';
+import { ItemTypeListComponent } from './item-type-list/item-type-list.component';
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { StorageService } from './storage.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemTypeComponent,
+    ItemTypeListComponent,
+    ItemComponent,
+    ItemListComponent,
+    ItemDetailComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    StorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
